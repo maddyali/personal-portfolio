77220124ca83a26f9714f7411aa7481aa22d3a71
Maddy Ali <maddyoali@gmail.com>
2025-08-13 09:51:16 -0500

    Update about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,35 +16,42 @@ export default function About() {
               className="h-auto w-full rounded-md"
             />
           </div>
-          <div className="flex flex-col gap-8">
-            <h2 className="text-2xl font-bold text-primary-dark leading-10 lg:text-3xl lg:text-left lg:ml-10">
-              About Me
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-bold text-gray-800 leading-10 lg:text-3xl lg:text-left lg:ml-10">
+              My Story
             </h2>
-            <p className="text-sm text-secondary-dark px-2 leading-6 sm:px-10 md:text-lg text-left">
-              I used to be an accountant. The problem? I liked when money went
-              missing. Now before you get any ideas, let me explain. See, it
-              gave me a problem to solve:
-            </p>
 
-            <p className="text-sm text-secondary-dark px-2 leading-6 sm:px-10 md:text-lg text-left">
-              • Was it a human error? <br />• A spreadsheet mistake? <br />•
-              Someone cooking the books?
-            </p>
+            <div className="text-md text-gray-600 leading-7 md:text-lg text-left lg:ml-10 space-y-4">
+              <p>
+                So I used to be an accountant. The problem? I liked when money
+                went missing. Now before you get any ideas, let me explain. You
+                see, it gave me a problem to solve:
+              </p>
 
-            <p className="text-sm text-secondary-dark px-2 leading-6 sm:px-10 md:text-lg text-left">
-              It was an introduction to the art of debugging. When I realized
-              software development is just testing, problem solving, and
-              building solutions–I was hooked. Now, instead of tracking down
-              missing money, I track down problems to solve using code.
-            </p>
+              <ul className=" list-disc list-inside space-y-1 ml-4">
+                <li>Was it human error?</li>
+                <li>A spreadsheet mistake?</li>
+                <li>Someone cooking the books?</li>
+              </ul>
 
-            <p className="text-sm text-secondary-dark px-2 leading-6 sm:px-10 md:text-lg text-left">
-              When I'm not building, you'll find me shooting hoops, reading with
-              an iced americano in hand, or creating educational content online.
-            </p>
-            <p className="font-semibold italic text-sm px-2 leading-6 sm:px-10 md:text-lg text-left text-secondary-dark">
-              My goal? To build things people love with people that inspire me.
-            </p>
+              <p>
+                It was my introduction to the art of debugging. When I realized
+                software development is just testing, problem solving, and
+                building solutions–I was hooked. Now, instead of tracking down
+                missing money, I track down problems to solve using code.
+              </p>
+
+              <p>
+                When I'm not building, you'll find me shooting hoops, reading
+                with an iced americano in hand, or creating educational content
+                online.
+              </p>
+
+              <p className="font-semibold italic text-gray-700">
+                My goal? To build things people love with people that inspire
+                me.
+              </p>
+            </div>
           </div>
         </div>
       </div>
